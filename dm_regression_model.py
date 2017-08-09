a307918e9fb4662677c254a10f69378f57dbf46c
Max Horn <maexlich@gmail.com>
2017-08-09 17:45:53 +0300

    Implemented optional initialization of alpha and beta

--- dm_regression_model.py
+++ dm_regression_model.py
@@ -5,7 +5,74 @@ import theano.tensor as tt
 from dirichlet_multinomial import DirichletMultinomial
 
 class DMRegressionModel(pm.Model):
-    def __init__(self, n_samples, n_covariates, n_otus, t0, nu=3, centered=True, cauchy=True,
+    def __init__(self, n_samples, n_covariates, n_otus, t0, nu=3, centered=True, cauchy=True, alpha_init=None,
+                 beta_init=None, name='', model=None):
+        super(DMRegressionModel, self).__init__(name, model)
+        self.S = n_samples
+        self.C = n_covariates
+        self.O = n_otus
+        self.covariates = theano.shared(np.zeros((self.S, self.C)), 'covariates')
+        self.data = theano.shared(np.ones((self.S, self.O), dtype=np.uint), 'data')
+        self.n = self.data.sum(axis=-1)
+
+        if alpha_init is None:
+            alpha_init=np.full(self.O, 0)
+        if beta_init is None:
+            beta_init=np.full((self.C, self.O), 0)
+
+        if cauchy:
+            if centered:
+                pm.HalfCauchy('tau', t0)
+            else:
+                tau_normal = pm.HalfNormal('tau-normal', t0)
+                tau_invGamma = pm.InverseGamma('tau-invGamma', 0.5, 0.5, testval=(0.5/(0.5+1)))
+                pm.Deterministic('tau', tau_normal*tt.sqrt(tau_invGamma))
+        else:
+            pm.HalfNormal('tau', t0)
+
+        if centered:
+            pm.HalfStudentT('lambda', nu=nu, mu=0, shape=(self.C, self.O))
+        else:
+            lamb_normal = pm.HalfNormal('lamb-Normal', 1, shape=(self.C, self.O))
+            lamb_invGamma = pm.InverseGamma('lamb-invGamma', 0.5 * nu, 0.5 * nu, shape=(self.C, self.O), testval=np.full((self.C, self.O), (0.5*nu)/(0.5*nu + 1)))
+            pm.Deterministic('lambda', lamb_normal * tt.sqrt(lamb_invGamma))
+
+        if centered:
+            pm.Normal('beta', 0, self['lambda']*self.tau, shape=(self.C, self.O), testval=beta_init)
+        else:
+            z = pm.Normal('z', 0, 1, shape=(self.C, self.O), testval=beta_init/beta_init.std())
+            pm.Deterministic('beta', z*self['lambda']*self.tau)
+
+        self.coefficient_mask = theano.shared(np.ones((self.C, self.O), dtype=np.uint8), 'beta_mask')
+        pm.Normal('alpha', 0, 10, shape=self.O, testval=alpha_init)
+        self.intermediate = tt.exp(self.alpha + tt.dot(self.covariates, self.beta*self.coefficient_mask))
+        DirichletMultinomial('counts', self.n, self.intermediate, shape=(self.S, self.O), observed=self.data)
+
+    def set_counts_and_covariates(self, counts, covariates):
+        if counts.shape[0] == covariates.shape[0]:
+            self.data.set_value(counts)
+            self.covariates.set_value(covariates)
+        else:
+            raise ValueError('Counts and covariates must have same sample dimension ({} vs {})'
+                             .format(counts.shape[0], covariates.shape[0]))
+
+    def set_counts(self, counts, borrow=False):
+        data_shape = self.data.shape.eval()
+        if counts.shape[0] == data_shape[0] and counts.shape[1] == data_shape[1]:
+            self.data.set_value(counts, borrow=borrow)
+        else:
+            raise ValueError('Counts must have same dimension ({}x{} vs {}x{})'
+                             .format(counts.shape[0], counts.shape[1], data_shape[0], data_shape[1]))
+
+    def set_coefficient_mask(self, coefficient_mask):
+        if coefficient_mask.shape[0] == self.C and coefficient_mask.shape[1] == self.O:
+            self.coefficient_mask.set_value(coefficient_mask)
+        else:
+            raise ValueError('Coefficient mask must have dimensions ({}x{})'.format(self.C, self.O))
+
+
+class DMRegressionMixed(pm.Model):
+    def __init__(self, n_samples, n_covariates, n_otus, t0, patients, nu=3, centered=True, cauchy=True,
                  name='', model=None):
         super(DMRegressionModel, self).__init__(name, model)
         self.S = n_samples
@@ -40,6 +107,7 @@ class DMRegressionModel(pm.Model):
 
         self.coefficient_mask = theano.shared(np.ones((self.C, self.O), dtype=np.uint8), 'beta_mask')
         pm.Normal('alpha', 0, 10, shape=self.O)
+
         self.intermediate = tt.exp(self.alpha + tt.dot(self.covariates, self.beta*self.coefficient_mask))
         DirichletMultinomial('counts', self.n, self.intermediate, shape=(self.S, self.O), observed=self.data)
 
@@ -64,6 +132,7 @@ class DMRegressionModel(pm.Model):
             self.coefficient_mask.set_value(coefficient_mask)
         else:
             raise ValueError('Coefficient mask must have dimensions ({}x{})'.format(self.C, self.O))
+
 
 
 class MaskableDMRegressionModel(DMRegressionModel):
